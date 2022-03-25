86bceb369a5c28f5a6869aa23cc1d62fb5660024
Martin <martin.world1977@gmail.com>
2022-03-24 21:14:01 -0400

    Added BrowserView.setVisible(), BrowserView.isVisible(), BrowserView.hide(), and BrowserView.show()

--- shell/browser/api/electron_api_browser_view.h
+++ shell/browser/api/electron_api_browser_view.h
@@ -20,6 +20,7 @@
 #include "shell/common/gin_helper/constructible.h"
 #include "shell/common/gin_helper/error_thrower.h"
 #include "shell/common/gin_helper/pinnable.h"
+#include "ui/gfx/image/image.h"
 
 namespace gfx {
 class Rect;
@@ -59,7 +60,7 @@ class BrowserView : public gin::Wrappable<BrowserView>,
   NativeWrapperBrowserView* owner_view() const { return owner_view_.get(); }
 
   void SetOwnerWindow(NativeWindow* window);
-void SetOwnerView(NativeWrapperBrowserView* view);
+  void SetOwnerView(NativeWrapperBrowserView* view);
 
   int32_t ID() const { return id_; }
 
@@ -68,7 +69,7 @@ void SetOwnerView(NativeWrapperBrowserView* view);
   BrowserView& operator=(const BrowserView&) = delete;
 
  protected:
-friend class WrapperBrowserView;
+  friend class WrapperBrowserView;
 
   BrowserView(gin::Arguments* args, const gin_helper::Dictionary& options);
   ~BrowserView() override;
@@ -93,6 +94,10 @@ friend class WrapperBrowserView;
   float GetScaleY();
   void SetOpacity(const double opacity, gin::Arguments* args);
   double GetOpacity();
+  void SetVisible(bool visible);
+  bool IsVisible();
+  void Hide(bool freeze, gfx::Image thumbnail);
+  void Show();
   v8::Local<v8::Value> GetWebContents(v8::Isolate*);
 #if BUILDFLAG(IS_MAC)
   void SetClickThrough(bool clickThrough);
@@ -105,6 +110,8 @@ friend class WrapperBrowserView;
   std::unique_ptr<NativeBrowserView> view_;
   base::WeakPtr<NativeWindow> owner_window_;
   scoped_refptr<NativeWrapperBrowserView> owner_view_;
+
+  bool page_frozen_ = false;
 
   int32_t id_;
 };
--- shell/browser/native_browser_view.h
+++ shell/browser/native_browser_view.h
@@ -13,8 +13,9 @@
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace gfx {
+class Image;
 class Rect;
-}
+}  // namespace gfx
 
 namespace gin_helper {
 class Dictionary;
@@ -68,6 +69,10 @@ class NativeBrowserView : public content::WebContentsObserver {
   virtual void SetOpacity(const double opacity,
                           const gin_helper::Dictionary& options) = 0;
   virtual double GetOpacity() = 0;
+  virtual void SetVisible(bool visible) = 0;
+  virtual bool IsVisible() = 0;
+  void ShowThumbnail(gfx::Image thumbnail);
+  void HideThumbnail();
 
   virtual void UpdateDraggableRegions(
       const std::vector<gfx::Rect>& drag_exclude_rects) {}
