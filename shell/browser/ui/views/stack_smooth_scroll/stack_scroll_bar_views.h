ff74d68df2d18a84681bf28a9b0cc090716b4644
Aleksander Zielony <olek.white.green@gmail.com>
2022-05-22 09:19:15 +0200

    [views] Refactor smooth scroll animation.

--- shell/browser/ui/views/stack_smooth_scroll/stack_scroll_bar_views.h
+++ shell/browser/ui/views/stack_smooth_scroll/stack_scroll_bar_views.h
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#ifndef ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_ELASTIC_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
-#define ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_ELASTIC_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
+#ifndef ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_SMOOTH_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
+#define ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_SMOOTH_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
 
 #include "ui/views/controls/scrollbar/scroll_bar_views.h"
 
-#include "electron/shell/browser/ui/views/stack_elastic_scroll/stack_elastic_scroll_animator.h"
+#include "electron/shell/browser/ui/views/stack_smooth_scroll/stack_smooth_scroll_animator.h"
 
 namespace electron {
 
@@ -27,9 +27,9 @@ class StackScrollBarViews : public views::ScrollBarViews {
   void OnGestureEvent(ui::GestureEvent* event) override;
 
  private:
-  std::unique_ptr<StackElasticScrollAnimator> animator_;
+  std::unique_ptr<StackSmoothScrollAnimator> animator_;
 };
 
 }  // namespace electron
 
-#endif  // ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_ELASTIC_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
+#endif  // ELECTRON_SHELL_BROWSER_UI_VIEWS_STACK_SMOOTH_SCROLL_STACK_SCROLL_BAR_VIEWS_H_
