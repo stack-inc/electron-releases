9ea72f2651594be43567d53ab16e47b08fea324b
Aleksander Zielony <olek.white.green@gmail.com>
2022-06-01 08:24:00 +0200

    [views] Add "smoothScroll" option to ScrollView

--- test/animate_zoomin_easein_scrollcontent.js
+++ test/animate_zoomin_easein_scrollcontent.js
@@ -81,7 +81,7 @@ function createWindow () {
   win.setContentBaseView(contentView);
 
   // Scroll
-  const scroll = new ScrollView();
+  const scroll = new ScrollView({ smoothScroll: true });
   //scroll.setStyle({
     //flex: 1,
   //});
