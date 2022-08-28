f349bbe67171583ad4a9915d893d591c984986a7
Aleksander Zielony <olek.white.green@gmail.com>
2022-08-28 10:09:26 +0200

    views: Fix SetRoundedCorners for NativeWebBrowserView

--- shell/browser/ui/native_web_browser_view_views.cc
+++ shell/browser/ui/native_web_browser_view_views.cc
@@ -1,6 +1,8 @@
 #include "shell/browser/ui/native_web_browser_view.h"
 
 #include "shell/browser/ui/inspectable_web_contents_view.h"
+#include "ui/compositor/layer.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/view.h"
 
 namespace electron {
@@ -35,5 +37,25 @@ void NativeWebBrowserView::UpdateClickThrough() {
   if (iwc_view)
     iwc_view->SetClickThrough(click_through);
 }
+
+void NativeWebBrowserView::SetRoundedCorners(
+    const RoundedCornersOptions& options) {
+  NativeView::SetRoundedCorners(options);
+  InspectableWebContentsView* iwc_view = GetInspectableWebContentsView();
+  if (!iwc_view)
+    return;
+  auto* view = iwc_view->GetView();
+  view->SetPaintToLayer();
+  view->layer()->SetFillsBoundsOpaquely(false);
+  float radius = options.radius;
+  auto corner_radii = gfx::RoundedCornersF(options.top_left ? radius : 0.0f,
+                                           options.top_right ? radius : 0.0f,
+                                           options.bottom_right ? radius : 0.0f,
+                                           options.bottom_left ? radius : 0.0f);
+  view->layer()->SetRoundedCornerRadius(corner_radii);
+  view->layer()->SetIsFastRoundedCorner(true);
+  iwc_view->SetCornerRadii(corner_radii);
+}
+
 
 }  // namespace electron
