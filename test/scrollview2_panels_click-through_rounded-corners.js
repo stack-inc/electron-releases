3d721c84a09edd7a434f807a6b1840495d579c2d
Daniel Kocielinski <daniel.koc@gmail.com>
2022-09-11 21:03:48 -0400

    views: Added blockScrollViewWhenFocus option to ScrollView API

--- test/scrollview2_panels_click-through_rounded-corners.js
+++ test/scrollview2_panels_click-through_rounded-corners.js
@@ -38,7 +38,7 @@ function createWindow () {
   win.setContentBaseView(contentView);
 
   // Scroll
-  const scroll = new ScrollView({ smoothScroll: true });
+  const scroll = new ScrollView({ smoothScroll: true, blockScrollViewWhenFocus: true });
   //scroll.setStyle({
     //flex: 1,
   //});
