c9e9f6560ba7497894aee9967e9e434537168118
Martin <martin.world1977@gmail.com>
2022-09-22 19:01:02 -0400

    hore: Added webContents.setCursor

--- shell/browser/api/electron_api_web_contents.h
+++ shell/browser/api/electron_api_web_contents.h
@@ -435,6 +435,7 @@ class WebContents : public ExclusiveAccessContext,
 
   void SetImageAnimationPolicy(const std::string& new_policy);
   gfx::Size GetPreferredSize();
+  void SetCursor(v8::Local<v8::Value> cursor_value, gin::Arguments* args);
 
   // disable copy
   WebContents(const WebContents&) = delete;
